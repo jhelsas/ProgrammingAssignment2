d623d1714f7f1f9bb72be4ed43505a87799682a2
jhelsas <jhelsas@gmail.com>
2016-10-02 20:05:32 -0400

    Presenting the solution to the Programming Assignment #2 of the 'R Programming' class in Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,43 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# This matrix create a cache-able matrix object
+# That can hold both a matrix and it's inverse
+# This solution builds upon the example fmakor the
+# cache mean calculations, specificaly, makeVector
 
+makeCacheMatrix <- function(m = matrix()) {
+  inv <- NULL
+  set <- function(inputm){
+    m <<- inputm
+    inv <<- NULL
+  }
+  get <- function() m
+  setinv <- function(inverse) inv <<- inverse # main addition
+  getinv <- function() inv 
+  list(set = set, get = get,
+       setinv = setinv, 
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+# This function returns the inverse of the cache-able
+# matrix object m, either from the previously cached
+# result, or calculating in the function call. 
+# In the later case, it stores the inverse in the 
+# inv component of the matrix object m.
+
+cacheSolve <- function(m, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- m$getinv()
+  if(!is.null(inv)){     # If the result was previously cached
+    message("getting cached data")
+    return(inv)          # just return
+  }
+  data <- m$get()        # else
+  inv <- solve(data,...) # calculate
+  m$setinv(inv)          # cache the result
+  inv                    # and return
 }
